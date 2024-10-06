c1f0b7644647e3603fbe0851bbda949a7627c27f
Rhys Miller <rjmills87@aol.com>
2024-10-06 21:18:55 +0100

    Style amendment to Project component.

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -10,7 +10,9 @@ export default function Project({ name, description, screenshot, alt, src }) {
       />
       <a href={src} className="flex items-center gap-3 py-4">
         <h3 className="text-2xl lg:text-3xl font-bold">{name}</h3>
-        <ArrowUpRightCircle color="#2563eb" strokeWidth={2} />
+        <span className="text-blue-600 rounded-full p-1 border-blue-600 border-1 transition duration-300 hover:bg-blue-600 hover:text-white">
+          <ArrowUpRightCircle strokeWidth={2} />
+        </span>
       </a>
       <p className="text-lg font-light">{description}</p>
     </>
