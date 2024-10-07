81d52cddf0c648b6808a9c9bb6cfdc638b47de7e
Rhys Miller <rjmills87@aol.com>
2024-10-07 19:58:50 +0100

    Create Contact Section and Contact Form component files.

---
+++ src/components/Contact.jsx
@@ -0,0 +1,9 @@
+export default function Contact() {
+  return (
+    <>
+      <h2 className="text-5xl lg:text-6xl font-bold pb-12 text-center">
+        Contact<span className="text-blue-600">.</span>
+      </h2>
+    </>
+  );
+}
