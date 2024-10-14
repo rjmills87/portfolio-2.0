0daa8aebf9f408ab70802ffe791a0c16d5ff499b
Rhys Miller <rjmills87@aol.com>
2024-10-14 21:49:25 +0100

    Added paragraph to Contact section on Homepage

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -4,9 +4,13 @@ export default function Contact() {
   return (
     <>
       <div id="contact" className="m-6 my-20 lg:mx-20">
-        <h2 className="text-5xl lg:text-6xl font-bold pb-12 text-center">
+        <h2 className="text-5xl lg:text-6xl font-bold pb-10 text-center">
           Contact<span className="text-blue-600">.</span>
         </h2>
+        <p className="text-center pb-10 text-lg">
+          I am available for freelance work. If you are interested in hiring me
+          for your project, please contact me.
+        </p>
         <ContactForm />
       </div>
     </>
