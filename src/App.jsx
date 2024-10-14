66a39af4c071448dee510f1cac6002fe0afb94a9
Rhys Miller <rjmills87@aol.com>
2024-10-14 09:02:58 +0100

    Ensure that page scrolls to the top on route change.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import projects from "./data/projects";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { useLayoutEffect } from "react";
 import Home from "./pages/Home";
 import ProjectPage from "./pages/ProjectPage";
 import Header from "./components/Header";
@@ -8,9 +9,20 @@ import Footer from "./components/Footer";
 import ScrollToTopButton from "./components/ScrollToTopButton";
 
 export default function App() {
+  const Wrapper = ({ children }) => {
+    const location = useLocation();
+
+    useLayoutEffect(() => {
+      // Scroll to the top of the page when the route changes
+      window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+    }, [location.pathname]);
+
+    return <>{children}</>;
+  };
+
   return (
-    <>
-      <BrowserRouter>
+    <BrowserRouter>
+      <Wrapper>
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
@@ -21,7 +33,7 @@ export default function App() {
         </Routes>
         <Footer />
         <ScrollToTopButton />
-      </BrowserRouter>
-    </>
+      </Wrapper>
+    </BrowserRouter>
   );
 }
